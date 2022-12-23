99227899d74543e3a6fe2acefb6743becaa8199f
Santrupti <105040967+lazybug19@users.noreply.github.com>
2022-12-23 17:55:22 +0530

    Update navigation_pad.ino

--- navigation_pad.ino
+++ navigation_pad.ino
@@ -14,7 +14,6 @@ void setup()
   pinMode(5,INPUT_PULLUP);
   pinMode(6,INPUT_PULLUP);
   pinMode(7,INPUT_PULLUP);
-  //Keyboard.begin();
 }
 
 void loop()
@@ -24,63 +23,52 @@ void loop()
  leftStatus=digitalRead(5);
  downStatus=digitalRead(6);
  rightStatus=digitalRead(7);
- 
- //UP ARROW PRESSED
+
  if (upStatus!=upStatusPrev && upStatus==LOW)
  {
-    //Keyboard.press('w');
     Serial.println('W');
     upStatusPrev=upStatus;
  }
- //UP ARROW RELEASED
+
  if (upStatus!=upStatusPrev && upStatus==HIGH)
  {
-    //Keyboard.release('w');
     Serial.println('w');
     upStatusPrev=upStatus;
  }
  
-  //LEFT ARROW PRESSED
  if (leftStatus!=leftStatusPrev && leftStatus==LOW)
  {
-    //Keyboard.press('a');
     Serial.println('A');
     leftStatusPrev=leftStatus;
  }
- //LEFT ARROW RELEASED
+
  if (leftStatus!=leftStatusPrev && leftStatus==HIGH)
  {
-    //Keyboard.release('a');
     Serial.println('a');
     leftStatusPrev=leftStatus;
  }
  
-  //DOWN ARROW PRESSED
+  
  if (downStatus!=downStatusPrev && downStatus==LOW)
  {
-    //Keyboard.press('s');
+   
     Serial.println('S');
     downStatusPrev=downStatus;
  }
- //DOWN ARROW RELEASED
+
  if (downStatus!=downStatusPrev && downStatus==HIGH)
  {
-    //Keyboard.release('s');
     Serial.println('s');
     downStatusPrev=downStatus;
  }
  
- //RIGHT ARROW PRESSED
  if (rightStatus!=rightStatusPrev && rightStatus==LOW)
  {
-    //Keyboard.press('j');
     Serial.println('J');
     rightStatusPrev=rightStatus;
  }
- //RIGHT ARROW RELEASED
  if (rightStatus!=rightStatusPrev && rightStatus==HIGH)
  {
-    //Keyboard.release('j');
     Serial.println('j');
     rightStatusPrev=rightStatus;
  }
